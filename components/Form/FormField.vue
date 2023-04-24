5cc85dca208373a04176f6fa934204b0163ef230
nknwns <andrejshaev@gmail.com>
2023-04-25 00:52:49 +0300

    feat(style), fix(components): add minimal form styles, fix select model

--- components/Form/FormField.vue
+++ components/Form/FormField.vue
@@ -44,3 +44,15 @@ export default {
   }
 }
 </script>
+
+<style>
+  .form__input {
+    background-color: #fff;
+    border: 1px solid #1B1917;
+    height: 35px;
+    padding: 5px 10px;
+    min-width: 260px;
+    outline: none;
+    font-size: 18px;
+  }
+</style>
--- components/Form/FormSelect.vue
+++ components/Form/FormSelect.vue
@@ -1,12 +1,15 @@
 <template>
   <select
+    @input="$emit('input', $event.target.value)"
     :name="name"
     :id="name"
     class="form__input form__input--select"
   >
+    <option disabled selected>{{ placeholder }}</option>
     <option
       v-for="option in options"
       :value="option.value"
+      :selected="option.value===value"
     >
       {{ option.text }}
     </option>
@@ -24,7 +27,23 @@ export default {
       name: {
         type: String,
         required: true
+      },
+      value: {
+        type: [String, Number, Boolean],
+        required: true
+      },
+      current: {
+        type: String,
+        default: ''
+      },
+      placeholder: {
+        type: String,
+        default: 'Выберите вариант'
       }
     }
 }
 </script>
+
+<style>
+
+</style>
