1ca8f4f91e160e84cf59a1192a8eb707dc51b1b8
nknwns <andrejshaev@gmail.com>
2023-04-25 00:58:40 +0300

    feat(structure): add pages structure

---
+++ pages/books/_id/index.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "index",
+    props: {},
+    data() {
+        return {}
+    },
+    computed: {},
+    methods: {},
+    created() {},
+    mounted() {}
+}
+</script>
---
+++ pages/books/index.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "index.vue",
+    props: {},
+    data() {
+        return {}
+    },
+    computed: {},
+    methods: {},
+    created() {},
+    mounted() {}
+}
+</script>
