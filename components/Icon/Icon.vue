aef890d9ea0bd3e2294d072bb5c34875bf3c1e98
nknwns <andrejshaev@gmail.com>
2023-04-25 03:58:35 +0300

    feat(components): create icon template

--- components/Icon/Icon.vue
+++ components/Icon/Icon.vue
@@ -1,5 +1,15 @@
 <template>
-
+  <svg xmlns="http://www.w3.org/2000/svg"
+       :width="width"
+       :height="height"
+       viewBox="0 0 24 24"
+       :aria-labelledby="name"
+       role="presentation"
+  >
+    <g :fill="iconColor">
+      <slot/>
+    </g>
+  </svg>
 </template>
 
 <script>
@@ -7,7 +17,24 @@
 
 export default {
   name: "Icon",
-  props: {},
+  props: {
+    name: {
+      type: String,
+      default: 'box'
+    },
+    width: {
+      type: [Number, String],
+      default: 24
+    },
+    height: {
+      type: [Number, String],
+      default: 24
+    },
+    iconColor: {
+      type: String,
+      default: 'currentColor'
+    }
+  },
   data() {
     return {}
   },
---
+++ components/Icon/IconArrow.vue
@@ -0,0 +1,21 @@
+<template>
+  <path d="M18.258 10.49952H0V13.49952H18.258L10.212 21.54555L12.333 23.66655L24 11.99952L12.333 0.33252L10.212 2.45352L18.258 10.49952Z"
+        :fill="fill"/>
+</template>
+
+<script>
+export default {
+  name: "IconArrow",
+  props: {
+    color: {
+      type: String,
+      default: 'currentColor'
+    }
+  },
+  computed: {
+    fill() {
+      return this.color ? this.color : 'currentColor'
+    }
+  }
+}
+</script>
