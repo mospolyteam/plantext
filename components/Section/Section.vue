cabf2af18cc96f410f0e040926b5abd337630014
nknwns <andrejshaev@gmail.com>
2023-04-25 03:59:58 +0300

    feat(components): create responsive section component

---
+++ components/Section/Section.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="row">
+        <h2 class="section__title">{{ title }}</h2>
+        <slot></slot>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Section",
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+  .section {
+    margin-bottom: 40px;
+  }
+
+  .section__title {
+    margin-bottom: 25px;
+  }
+</style>
