e3efacb65bb4b84f5de6ca87ea2cd00146a22538
nami <warriorparovoz@gmail.com>
2023-04-26 11:58:08 +0300

    chore: create discussion and partner components

---
+++ components/Discussion/Discussion.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "Discussion",
+    props: {},
+    data() {
+        return {}
+    },
+    computed: {},
+    methods: {},
+    created() {},
+    mounted() {}
+}
+</script>
