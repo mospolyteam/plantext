9e99a08ff3858e70c61d10a9aa384d28dcd67d29
nami <warriorparovoz@gmail.com>
2023-04-26 18:05:44 +0300

    feat(components) create slider and sliderItem

--- components/Slider/Slider.vue
+++ components/Slider/Slider.vue
@@ -1,17 +1,167 @@
 <template>
-  <div></div>
+  <div class="container">
+    <div class="slider">
+      <button class="slider__navigation slider__navigation--left"
+        :disabled="inProcess"
+        @click="previousSlide"
+      >
+        <img src="previous-slide.svg">
+      </button>
+      <slider-item
+        v-for="slide in slides"
+        :class="slidePosition(slide.id)"
+        :key="slide.id"
+        :titleColor="slide.titleColor"
+        :childPosition="slide.childPosition"
+        :cover="slide.cover"
+      >
+        <template v-slot:title>
+          <p :class="`slide__title slide__title--${slide.titlePosition}`"
+            :style="{color: slide.titleColor}"
+          >
+            {{ slide.title }}
+          </p>
+        </template>
+        <template v-slot:child>
+          <div :class="`slide__child slide__child--${slide.childPosition}`">
+            <input type="text"/>
+              <Button 
+                label="Подписаться"
+                color="purple"
+              >
+                Подписаться
+              </Button>
+          </div>
+        </template>
+      </slider-item>
+      <button class="slider__navigation slider__navigation--right"
+        :disabled="inProcess"
+        @click="nextSlide"
+      >
+        <img src="next-slide.svg">
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
+import Button from "~/components/Button/Button.vue";
+
 export default {
-    name: "Slider",
-    props: {},
-    data() {
-        return {}
+  name: "Slider",
+  components: {
+    Button
+  },
+  props: {},
+  data() {
+    return {
+      currentSlide: 1,
+      inProcess: false,
+      slides: [
+        {
+          id: 1,
+          title: "Подпишитесь на нашу рассылку",
+          titleColor: "#F2F1E9",
+          titlePosition: 1,
+          childPosition: 4,
+          cover: "slide-mail.jpg",
+        },
+        {
+          id: 2,
+          title: "Подпишитесь на нашу рассылку",
+          titleColor: "#F2F1E9",
+          titlePosition: 2,
+          childPosition: 3,
+          cover: "slide-mail.jpg",
+        },
+        {
+          id: 3,
+          title: "Подпишитесь на нашу рассылку",
+          titleColor: "#F2F1E9",
+          titlePosition: 3,
+          childPosition: 1,
+          cover: "slide-mail.jpg",
+        },
+        {
+          id: 4,
+          title: "Подпишитесь на нашу рассылку",
+          titleColor: "#F2F1E9",
+          titlePosition: 4,
+          childPosition: 2,
+          cover: "slide-mail.jpg"
+        }
+      ]
+    }
+  },
+  watch: {
+    currentSlide() {
+      if (this.currentSlide > this.slides.length) {
+        this.currentSlide = 1
+      }
+      if (this.currentSlide <= 0) {
+        this.currentSlide = this.slides.length
+      }
+    }
+  },
+  computed: {
     },
-    computed: {},
-    methods: {},
-    created() {},
-    mounted() {}
+  methods: {
+    slidePosition(id) {
+      if (id === this.currentSlide) {
+        return 'active'
+      } else if (
+        (id === this.currentSlide + 1) || 
+        ((this.currentSlide === this.slides.length) && (id === 1))
+      ) {
+        return 'next'
+      } else if (
+        (id === this.currentSlide - 1) || 
+        ((this.currentSlide === 1) && (id === this.slides.length))
+      ) {
+        return 'previous'
+      }
+      return ''
+    },
+    nextSlide() {
+      this.setInProcess()
+      this.currentSlide++
+    },
+    previousSlide() {
+      this.setInProcess()
+      this.currentSlide--
+    },
+    setInProcess() {
+      this.inProcess = true
+      setTimeout(() => {
+        this.inProcess = false
+      }, 1000)
+    },
+  },
+  created() {},
+  mounted() {}
 }
 </script>
+
+<style>
+  .slider {
+    position: relative;
+    height: 330px;
+    overflow: hidden;
+  }
+  .slider__navigation {
+    z-index: 1;
+    position: absolute;
+    width: 32px;
+    height: 32px;
+    background: #D95E32;
+    cursor: pointer;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  .slider__navigation--left {
+    left: 20px;
+  }
+  .slider__navigation--right {
+    right: 20px;
+  }
+</style>
--- components/Slider/SliderItem.vue
+++ components/Slider/SliderItem.vue
@@ -1,11 +1,17 @@
 <template>
-
+  <div class="slider__slide slide">
+    <img class="slide__cover" :src="`/images/${cover}`">
+    <slot name="title" />
+    <slot name="child" />
+  </div>
 </template>
 
 <script>
 export default {
     name: "SliderItem",
-    props: {},
+    props: {
+      cover: String,
+    },
     data() {
         return {}
     },
@@ -15,3 +21,53 @@ export default {
     mounted() {}
 }
 </script>
+
+<style>
+  .slide {
+    position: absolute;
+    visibility: hidden;
+    transform: scale(0.75);
+  }
+  .next {
+    visibility: hidden;
+    left: 100%;
+    transition: left 1s, visibility 1s, transform 1s ease-in;
+  }
+  .previous {
+    visibility: hidden;
+    left: -100%;
+    transition: left 1s, visibility 1s, transform 1s ease-in;
+  }
+  .active {
+    transform: scale(1);
+    visibility: visible;
+    left: 0%;;
+    transition: left 1s, transform 1s ease-out;
+  }
+  .slide__title, .slide__child {
+    position: absolute;
+    max-width: 40%;
+    max-height: 40%;
+    font-size: 36px;
+    color: white;
+  }
+  .slide__child {
+    display: flex;
+  }
+  .slide__title--1, .slide__child--1 {
+    top: 30px;
+    left: 102px;
+  }
+  .slide__title--2, .slide__child--2 {
+    top: 30px;
+    right: 102px;
+  }
+  .slide__title--3, .slide__child--3 {
+    bottom: 30px;
+    left: 102px;
+  }
+  .slide__title--4, .slide__child--4 {
+    bottom: 30px;
+    right: 102px;
+  }
+</style>
