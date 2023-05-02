52637c95c4ad4177075af8fa1d05f61032ad5590
nknwns <andrejshaev@gmail.com>
2023-05-02 10:05:36 +0300

    feat(components): add breadcrumbs (without current page if path include digit)

--- components/Breadcrumbs/Breadcrumbs.vue
+++ components/Breadcrumbs/Breadcrumbs.vue
@@ -1,19 +1,57 @@
 <template>
-
+  <div class="breadcrumbs">
+    <ul class="breadcrumbs__list">
+      <BreadcrumbsItem
+        v-for="(item, i) in items"
+        :key="i"
+        :link="item.link"
+      >
+        {{ item.text }}
+      </BreadcrumbsItem>
+    </ul>
+  </div>
 </template>
 
 <script>
-// TODO: Реализовать компоненту (Андрей)
+import BreadcrumbsItem from "~/components/Breadcrumbs/BreadcrumbsItem.vue";
 
+const routesDictionary = {
+  'books': 'Книги',
+  'discussions': 'Обсуждения',
+  'articles': 'Статьи'
+}
 export default {
-    name: "Breadcrumbs",
-    props: {},
-    data() {
-        return {}
-    },
-    computed: {},
-    methods: {},
-    created() {},
-    mounted() {}
+  name: "Breadcrumbs",
+  components: {BreadcrumbsItem},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {
+    items() {
+      let routes = [
+        {
+          link: '/',
+          text: 'Главная'
+        }
+      ];
+
+      return this.$route.path.split('/').reduce((accum, current, index) => {
+        return [...accum, this.createRouteElement(accum[index].link + current + '/', current)];
+      }, routes);
+    }
+  },
+  methods: {
+    createRouteElement(link, current) {
+      // TODO: Сделать обработку случаев, когда текущий путь - это цифра. Необходимо достать название статьи/книги
+      // Есть две идеи. Или брать название в отдельный props, или вытаскивать из API каждый раз.
+      return {
+        link: link,
+        text: routesDictionary[current]
+      }
+    }
+  },
+  mounted() {
+  }
 }
 </script>
