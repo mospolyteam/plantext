d553f5fcce34f8365ddb4b34713e9e09189aaa68
Ollocreate <neustroeva-olenka@mail.ru>
2023-05-04 14:48:02 +0300

    feat(components): add Row and Logo

---
+++ components/Header/HeaderFunction.vue
@@ -0,0 +1,14 @@
+<template>
+  <li class="header__function">
+    <button class="header__button" @click="$emit('click')">
+      <slot/>
+    </button>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "HeaderFunction",
+  emits: ['click']
+}
+</script>
